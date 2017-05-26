e936d1bc270fb9203f0fd35b30f127c4498bf4c9
Al Sierra <al@cloudred.com>
2017-05-26 14:03:44 -0400

    Add controller that is global for the entire app

---
+++ web/app/themes/mjh/resources/controllers/app.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+
+class App extends Controller
+{
+    public function siteName()
+    {
+        return get_bloginfo('name');
+    }
+
+    public function siteLogo()
+    {
+        $custom_logo_id = get_theme_mod( 'custom_logo' );
+		$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+		return $image;
+	}
+
+}
