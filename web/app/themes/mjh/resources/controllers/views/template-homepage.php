e79b6f7c3cce7bce4cd4fc474245a71b7b8855dd
Al Sierra <al@cloudred.com>
2017-05-26 11:16:02 -0400

    Add homepage template Add homepage controller Set up social media links in header

---
+++ web/app/themes/mjh/resources/controllers/views/template-homepage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+use WP_Query;
+
+class Homepage extends Controller
+{
+    /**
+     * Return all exhibitions posts
+     *
+     * @return array
+     */
+    public function exhibitions() {
+      $exhibitions = new WP_Query( [ 'post_type' => 'exhibitions' ] );
+      return $exhibitions;
+    }
+
+    /**
+     * Return images from Advanced Custom Fields
+     *
+     * @return array
+     */
+    public function images()
+    {
+        return get_field('images');
+    }
+}
--- web/app/themes/mjh/resources/views/partials/header.blade.php
+++ web/app/themes/mjh/resources/views/partials/header.blade.php
@@ -1,5 +1,18 @@
 <header class="banner">
   <div class="container">
+    <div class="social-channels">
+      <a href="https://www.facebook.com/MuseumofJewishHeritage" target="_blank" onclick="return trackOutboundLink('https://www.facebook.com/MuseumofJewishHeritage', true)">
+        <i class="fa fa-facebook" aria-hidden="true"></i></a>
+         &nbsp; &nbsp;
+      <a href="https://twitter.com/mjhnews" target="_blank" onclick="return trackOutboundLink('https://twitter.com/mjhnews', true)">
+        <i class="fa fa-twitter" aria-hidden="true"></i></a>
+        &nbsp; &nbsp;
+      <a href="https://www.instagram.com/museumjewishheritage/" target="_blank" onclick="return trackOutboundLink('https://www.instagram.com/museumjewishheritage/', true)">
+        <i class="fa fa-instagram" aria-hidden="true"></i></a>
+        &nbsp; &nbsp;
+      <a href="https://www.youtube.com/MuseumJewishHeritage/" target="_blank" onclick="return trackOutboundLink('https://www.youtube.com/MuseumJewishHeritage/', true)">
+        <i class="fa fa-youtube" aria-hidden="true"></i></a>
+    </div>
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
