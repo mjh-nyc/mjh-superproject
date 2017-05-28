9259a81f812e3a751f5d7183fdd4fb406499e647
Cyril Tsiboulski <ct@cloudred.com>
2017-05-28 15:30:25 -0400

    Added navs

--- web/app/themes/mjh/resources/views/partials/header.blade.php
+++ web/app/themes/mjh/resources/views/partials/header.blade.php
@@ -15,25 +15,41 @@
           <i class="fa fa-youtube" aria-hidden="true"></i></a>
       </div>
       <nav class="nav-secondary">
-        <ul>
+       @if (has_nav_menu('minitop_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'minitop_navigation']) !!}
+        @endif
+        <!--<ul>
           <li><a href="">Blog</a></li>
           <li><a href="">Press</a></li>
           <li><a href="">JewishGen</a></li>
           <li><a href="">Space Rental</a></li>
           <li><a href="">Donate</a></li>
-        </ul>
+        </ul>-->
       </nav>
     </div>
     <a class="brand" href="{{ home_url('/') }}">{!!  get_custom_logo() !!}</a>
-    <div class="actions">
+    
+    @if (has_nav_menu('buttontop_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'buttontop_navigation', 'menu_class' => 'actions']) !!}
+    @endif
+    <!--<div class="actions">
+
       <a href="" class="cta sqr">Join</a>
       <a href="" class="cta sqr">Buy Tickets</a>
       <a href="" class="toggle"><i class="fa fa-bars" aria-hidden="true"></i></a>
-    </div>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+    </div>-->
+    <div class="overlay-nav">
+      <div class="site-search">
+        {!! get_search_form(false) !!}
+      </div>
+      <nav class="nav-primary">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
+      </nav>
+      @if (has_nav_menu('minitop_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'minitop_navigation']) !!}
       @endif
-    </nav>
+    </div>
   </div>
 </header>
