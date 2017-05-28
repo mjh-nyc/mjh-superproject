70e4e7cd01b6041de94c5e21c24dad55f3d31d1d
Cristian Zapata <cristian_zapata@comcast.com>
2017-05-28 13:42:48 -0400

    First pass at footer structure

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -1,5 +1,54 @@
-<footer class="content-info">
+<footer class="content-info container-fluid">
   <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
+
+    <h4>
+      Museum of Jewish Heritage
+      A Living Memorial to the Holocaust
+    </h4>
+
+    <div class="row">
+      <div class="box">
+
+        <h5>Find Us</h5>
+
+        <p>Edmond J. Safra Plaza
+        36 Battery Place
+        New York, NY 10280
+        (646) 437–4202</p>
+      </div>
+
+      <div class="box">
+        <h5>Stay in Touch!</h5>
+        <p>Sign up with your email to receive news, updates and exclusive event invitations from the Museum of Jewish Heritage.</p>
+        Sign up
+      </div>
+
+      <div class="box">
+        <h5>Supporters</h5>
+
+        <p>Suspendisse vehicula nisl quis enim sagittis, nec mollis purus lacinia. In fringilla vel orci eu sagittis.</p>
+
+      </div>
+      <div class="box">
+        <h5>Learn more</h5>
+        <p>Visit Our Resident Theater Company</p>
+      </div>
+
+    </div>
+
+    <div class="copyright row">
+      <nav class="links">
+        <ul>
+          <li><a href="">Contact</a></li>      
+          <li><a href="">Jobs</a></li>      
+          <li><a href="">Sitemap</a></li>     
+          <li><a href="">Terms of Use</a></li>
+        </ul>
+      </nav>
+      <div class="content">
+        <p>&copy; 2017 Museum of Jewish Heritage—A Living Memorial to the Holocaust. All rights reserved. </p>
+      </div>
+    </div>
   </div>
 </footer>
