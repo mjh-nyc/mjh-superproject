88b66d8807178cebfba98a717d727a037b324961
Cristian Zapata <cristian_zapata@comcast.com>
2017-05-29 13:03:18 -0400

    TEMP: Bentosans import

--- web/app/themes/mjh/resources/views/partials/head.blade.php
+++ web/app/themes/mjh/resources/views/partials/head.blade.php
@@ -3,4 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php(wp_head())
+
+  <!-- REMOVE WHEN TYPE KIT IS INSTALLED -->
+  <link href="//cloud.typenetwork.com/projects/1448/fontface.css/" rel="stylesheet" type="text/css">
+  <!-- REMOVE WHEN TYPE KIT IS INSTALLED -->
 </head>
