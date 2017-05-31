d773b1156cc5b62c178a8000d4bebfc36fd46f32
Cyril Tsiboulski <ct@cloudred.com>
2017-05-31 15:25:58 -0400

    Added the find us widget

--- web/app/themes/mjh/app/extras.php
+++ web/app/themes/mjh/app/extras.php
@@ -207,3 +207,69 @@ function hook_meta() {
 }
 add_action('wp_head', 'App\\hook_meta');
 /* END meta tags ****************************/
+
+
+/********************************************/
+//Find Us / Address widget
+class find_us extends \WP_Widget {
+	function __construct() {
+		parent::__construct(
+			'findus_widget', // Base ID
+			esc_html__( 'Address widget', 'sage' ), // Name
+			array( 'description' => esc_html__( 'Simple widget to display museum’s address', 'sage' ), )
+		);
+	}
+	/**
+	* Front-end display of widget.
+	*
+	* @see WP_Widget::widget()
+	*
+	* @param array $args     Widget arguments.
+	* @param array $instance Saved values from database.
+	*/
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+		echo esc_html__( 'Pull museum address here', 'sage' );
+		echo $args['after_widget'];
+	}
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'sage' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'sage' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+}
+add_action( 'widgets_init', function(){
+     register_widget( 'App\\find_us' );
+});
+/* END find us widget  ****************************/
