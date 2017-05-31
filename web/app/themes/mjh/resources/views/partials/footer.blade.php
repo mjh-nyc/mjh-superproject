84fb77b047ab7418bbb217b3be3c7f660234741a
Cyril Tsiboulski <ct@cloudred.com>
2017-05-31 15:26:51 -0400

    Reworked footer to use widgets

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -1,34 +1,11 @@
 <footer class="content-info container-fluid">
   <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
     <h3>
       {{App::siteName()}}
     </h3>
+
     <div class="row">
-      <div class="box">
-        <h5>Find Us</h5>
-        <p>Edmond J. Safra Plaza<br>
-        36 Battery Place<br>
-        New York, NY 10280<br>
-        (646) 437–4202</p>
-      </div>
-
-      <div class="box">
-        <h5>Stay in Touch!</h5>
-        <p>Sign up with your email to receive news, updates and exclusive event invitations from the Museum of Jewish Heritage.</p>
-        <a href="" class="cta">Sign up</a>
-      </div>
-
-      <div class="box">
-        <h5>Supporters</h5>
-        <p>Suspendisse vehicula nisl quis enim sagittis, nec mollis purus lacinia. In fringilla vel orci eu sagittis.</p>
-        <a href="" class="cta">Learn more</a>
-      </div>
-      <div class="box">
-        
-        <h5>Visit Our Resident Theater Company</h5>
-      </div>
-
+      @php(dynamic_sidebar('sidebar-footer'))
     </div>
 
     <div class="copyright row">
@@ -36,7 +13,7 @@
         {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'container_class' => 'links']) !!}
       @endif
       <div class="content">
-        <p>&copy; 2017 Museum of Jewish Heritage—A Living Memorial to the Holocaust. All rights reserved. </p>
+        <p>&copy; @php echo date("Y"); @endphp {{App::siteName()}}. @php _e("All Rights Reserved.","sage"); @endphp</p>
       </div>
     </div>
   </div>
