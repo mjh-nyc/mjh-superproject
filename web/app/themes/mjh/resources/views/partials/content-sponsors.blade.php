b1e5cac4c4030ad4960213746f62f11c5c0d130d
Cristian Zapata <cristian_zapata@comcast.com>
2017-06-01 23:02:03 -0400

    Sponsors tweaks

--- web/app/themes/mjh/resources/views/partials/content-sponsors.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-sponsors.blade.php
@@ -1,15 +1,15 @@
 @if (App::get_repeater_field('primary_sponsors_repeater'))
-<div class="{{ $sectionType }}-sponsors {{ $sectionClass }}-sponsors">
+<div class="{{ $sectionType }}-sponsors {{ $sectionClass }}-sponsors sponsors">
   <h4 class="subhead">{{$sectionTitle}}</h4>
   <ul>
   @foreach (App::get_repeater_field( $sectionType.'_sponsors_repeater') as $sponsors)
     <li>
+      @if ($sponsors['sponsor_url'])<a href="{{ $sponsors['sponsor_url'] }}" target="_blank">@endif<div class="sponsor-name">{{$sponsors['sponsor_name']}}</div>@if ($sponsors['sponsor_url'])</a>@endif
       @if ($sponsors['sponsor_image'])
       <div class="{{ $sectionType }}-sponsor-image image">
         @if ($sponsors['sponsor_url'])<a href="{{ $sponsors['sponsor_url'] }}" target="_blank">@endif <img src="{!! App::getAttachmentById($sponsors['sponsor_image']['ID'],'thumbnail') !!}"> @if ($sponsors['sponsor_url'])</a>@endif
       </div>
       @endif
-      @if ($sponsors['sponsor_url'])<a href="{{ $sponsors['sponsor_url'] }}" target="_blank">@endif<div class="sponsor-name">{{$sponsors['sponsor_name']}}</div>@if ($sponsors['sponsor_url'])</a>@endif
     </li>
   @endforeach
   </ul>
