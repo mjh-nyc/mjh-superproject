b0233dd91d0c9ef4d2201463db671e44d7e699aa
Cyril Tsiboulski <ct@cloudred.com>
2017-06-03 13:23:11 -0400

    Added case for adding no-sponsors class name to article element

--- web/app/themes/mjh/resources/views/partials/content-single-event.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-single-event.blade.php
@@ -1,4 +1,5 @@
-<article @php(post_class())>
+
+<article @php(post_class(App::addSponsorsClass()))>
   <div class="col-content row">
 
     <div class="right-sidebar">
@@ -44,17 +45,20 @@
       @include('partials.content-related-links')
     </div>
 
-    <div class="left-sidebar">
-      @if (App::get_repeater_field('primary_sponsors_repeater'))
-        <!-- Primary sponsors -->
-        @include('partials.content-sponsors', ['sectionTitle' => App::get_field('primary_sponsor_header'),'sectionClass'=>"exhibition",'sectionType'=>"primary"])
-      @endif
+     @if (App::get_repeater_field('primary_sponsors_repeater') || App::get_repeater_field('secondary_sponsor_header'))
+      <div class="left-sidebar">
+        
+        @if (App::get_repeater_field('primary_sponsors_repeater'))
+          <!-- Primary sponsors -->
+          @include('partials.content-sponsors', ['sectionTitle' => App::get_field('primary_sponsor_header'),'sectionClass'=>"exhibition",'sectionType'=>"primary"])
+        @endif
 
-      @if (App::get_repeater_field('secondary_sponsors_repeater'))
-        <!-- Secondary sponsors -->
-        @include('partials.content-sponsors', ['sectionTitle' => App::get_field('secondary_sponsor_header'),'sectionClass'=>"exhibition",'sectionType'=>"secondary"])
-      @endif
-    </div>
+        @if (App::get_repeater_field('secondary_sponsor_header'))
+          <!-- Secondary sponsors -->
+          @include('partials.content-sponsors', ['sectionTitle' => App::get_field('secondary_sponsor_header'),'sectionClass'=>"exhibition",'sectionType'=>"secondary"])
+        @endif
+      </div>
+    @endif
 
   </div>
   <footer>
