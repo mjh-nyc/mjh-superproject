c5437ccc8955131accbfdbb474aa928bd7740797
Cyril Tsiboulski <ct@cloudred.com>
2017-06-02 23:16:08 -0400

    Added caption to gallery template

--- web/app/themes/mjh/resources/views/partials/content-gallery.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-gallery.blade.php
@@ -1,9 +1,15 @@
 @if (App::get_field('image_slideshow'))
 	<div class="gallery">
 		@foreach( App::get_field('image_slideshow') as $image )
-            <div class="slide" style="background-image: url('{{ $image['sizes']['medium'] }}'); height:350px;">
-               <span class="sr-only">{{ $image['alt'] }}"</span>
+            <div class="slide">
+            	<div style="background-image: url('{{ $image['sizes']['medium'] }}'); height:350px;">
+               		<span class="sr-only">{{ $image['alt'] }}"</span>
+               	</div>
+               	<div class="gallery-slide-caption">
+            		{{ $image['caption'] }}
+            	</div>
             </div>
+
         @endforeach
     </div>
 @endif
