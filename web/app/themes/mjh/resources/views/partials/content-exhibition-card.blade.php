241392da849e02bcae38f2b1687054cee9205df7
Cristian Zapata <cristian_zapata@comcast.com>
2017-06-03 22:38:40 -0400

    Event cards on homepage - and made the whole card clickable

--- web/app/themes/mjh/resources/views/partials/content-exhibition-card.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-exhibition-card.blade.php
@@ -1,8 +1,9 @@
 {{-- You must pass the post ID to this template as $item_id --}}
 <div class="slide-card exhibtion-card">
 	<!-- Hero bg in header template -->
+	<a href="{!! get_the_permalink($item_id); !!}" class="card-link">
 	<div class="card-image"><div class="wrap">
-		<span><a href="{!! get_the_permalink($item_id); !!}">{!! App::featuredImage('square@1x',$item_id) !!}</a></span>
+		<span>{!! App::featuredImage('square@1x',$item_id) !!}</span>
 	</div></div>
 	<h3>{{ get_the_title($item_id) }}</h3>
 	<p class="description">{{App::postExcerpt($item_id)}}</p>
@@ -12,4 +13,5 @@
 			<p>{{App::get_field('exhibition_start_date',$item_id)}} &#8211; {{App::get_field('exhibition_end_date',$item_id)}}</p>
 		@endif
 	</div>
+	</a>
 </div>
