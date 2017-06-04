e209387883b70e4dd4d027e05f85bb809b567a15
Cristian Zapata <cristian_zapata@comcast.com>
2017-06-03 21:03:55 -0400

    Post Details

--- web/app/themes/mjh/resources/views/partials/page-header.blade.php
+++ web/app/themes/mjh/resources/views/partials/page-header.blade.php
@@ -1,7 +1,7 @@
 <div class="page-header">
   <h1>{!! App\title() !!}</h1>
   @if ( !empty($post) && $post->post_type =='post')
-    <div>@php(the_date('l, F j, Y'))</div>
+    <div class="post-date">@php(the_date('l, F j, Y'))</div>
   @endif
   @if (is_search())
      <p class="results-stats"><span class="bold">{{ Search::resultsFound() }}</span> results for &#8220; {!! get_search_query() !!} &#8221;</p>
