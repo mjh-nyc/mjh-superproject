b3b577a4a3ba0a5297f2dcea36eda8a42eedfa37
Cyril Tsiboulski <ct@cloudred.com>
2017-06-05 01:44:55 -0400

    Added google analytics updated styles for social links in overlay nav

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -18,3 +18,24 @@
     </div>
   </div>
 </footer>
+<script>
+//Google Analytics Code
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-29738632-1', 'auto');
+        ga('send', 'pageview');
+
+        var trackOutboundLink = function(url, isExternal) {
+            var params = {};
+          if (!isExternal) {
+              params.hitCallback = function () {
+                  document.location = url;
+              }
+          }
+          ga('send', 'event', 'outbound', 'click', url, params);
+          return isExternal;
+          }
+</script>
