5d71966227026ca120b10f54bc0a59ff5429a551
Cyril Tsiboulski <ct@cloudred.com>
2017-06-05 01:00:38 -0400

    Working on share links

--- web/app/themes/mjh/resources/views/partials/content-share.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-share.blade.php
@@ -4,13 +4,13 @@
 	</div>
 	<div class="share-options">
 		<div class="share-channel">
-			<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="https://www.facebook.com/sharer/sharer.php?u={!! get_the_permalink(); !!}" target="_blank" onclick="ga('send', 'event', 'social', 'share', 'facebook');"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 		</div>
 		<div class="share-channel">
-			<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+			<a href="https://twitter.com/intent/tweet?text={!! urlencode(App::postTitle()); !!}&via={!! App::get_field('twitter_handle', 'option') !!}" onclick="ga('send', 'event', 'social', 'share', 'twitter');" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
 		</div>
 		<div class="share-channel">
-			<a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+			<a href="https://pinterest.com/pin/create/button/?url={!! get_the_permalink(); !!}&media={!! App::featuredImageSrc(); !!}&description={!! urlencode(App::postTitle()); !!}" onclick="ga('send', 'event', 'social', 'share', 'pinterest');" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
 		</div>
 		<div class="share-channel">
 			<a href="#"><i class="fa fa-tumblr" aria-hidden="true"></i></a>
