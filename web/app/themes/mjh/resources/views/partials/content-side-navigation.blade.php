a6efd6a7e42a7d7a821a1b11ed580d8399e3269d
Cristian Zapata <cristian_zapata@comcast.com>
2017-06-12 20:58:07 -0400

    sub nav

--- web/app/themes/mjh/resources/views/partials/content-side-navigation.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-side-navigation.blade.php
@@ -1,10 +1,12 @@
 @if (App::getSubPageNav())
-	<ul>
-		<!-- print top level page -->
-		<li @if (App::is_ancestor(get_the_ID())) class="current" @endif><a href="{{ get_the_permalink(App::get_parent_id(get_the_ID())) }}">{{ App::postTitle(App::get_parent_id(get_the_ID())) }}</a></li>
-		<!-- subpages -->
-		@foreach (App::getSubPageNav() as $page)
-			 <li @if ($page->ID === get_the_ID()) class="current" @endif><a href="{{ the_permalink($page->ID) }}">{!! $page->post_title !!}</a></li>
-		@endforeach
-	</ul>
+	<div class="subPageNav">
+		<ul>
+			<!-- print top level page -->
+			<li @if (App::is_ancestor(get_the_ID())) class="current" @endif><a href="{{ get_the_permalink(App::get_parent_id(get_the_ID())) }}">{{ App::postTitle(App::get_parent_id(get_the_ID())) }}</a></li>
+			<!-- subpages -->
+			@foreach (App::getSubPageNav() as $page)
+				<li @if ($page->ID === get_the_ID()) class="current" @endif><a href="{{ the_permalink($page->ID) }}">{!! $page->post_title !!}</a></li>
+			@endforeach
+		</ul>
+	</div>
 @endif
