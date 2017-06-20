e0706089e84f7791c365ac0964a6da7cfbef1146
Al Sierra <al@cloudred.com>
2017-06-20 16:03:28 -0400

    Add blog listing templates

---
+++ web/app/themes/mjh/resources/views/template-blog-listing.blade.php
@@ -0,0 +1,28 @@
+{{--
+  Template Name: Blog Listing
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+      @if($blogs)
+      <div class="listing-wrapper row">
+        @foreach ($blogs as $blog_post)
+          <article @php(post_class())>
+            @include('partials.content-blog-card', ['item_id'=>$blog_post->ID])
+          </article>
+        @endforeach
+      @else
+        <div style="margin-bottom: 100px;">
+          <div class="alert alert-warning">@php _e("There are no events to display","sage"); @endphp</div>
+          {!! get_search_form(false) !!}
+        </div>
+      @endif
+      </div>
+      @if ($get_max_num_pages)
+        @include('partials.pagination',['max_num_pages'=>$get_max_num_pages])
+      @endif
+    @include('partials.content-page')
+  @endwhile
+@endsection
