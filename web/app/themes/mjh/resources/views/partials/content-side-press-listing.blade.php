1454490efe7764503f0d8367cf84f7ca5b2fef16
Cyril Tsiboulski <ct@cloudred.com>
2017-06-22 11:39:52 -0400

    Styled sticky press items

--- web/app/themes/mjh/resources/views/partials/content-side-press-listing.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-side-press-listing.blade.php
@@ -1,12 +1,15 @@
-@if (App::getStickyPosts())
-  <h3>@php _e("Museum Press  ","sage"); @endphp</h3>
+<div class="press-content-aside col-md-4">
+	<h3>
+		@php _e("Museum Press","sage"); @endphp
+	</h3>
 	<div class="side press-list">
 		<ul>
 			@foreach (App::getStickyPosts() as $post_id )
-			  @if(has_category( 'press', $post_id ))
-				<li @if ($post_id === get_the_ID()) class="current" @endif><a href="{{ the_permalink($post_id ) }}">{!! get_the_title($post_id) !!}</a></li>
+				@if(has_category( 'press', $post_id ))
+					<li @if ($post_id === get_the_ID()) class="current" @endif><a href="{{ the_permalink($post_id ) }}">{!! get_the_title($post_id) !!}</a></li>
 				@endif
 			@endforeach
 		</ul>
 	</div>
-@endif
+</div>
+
--- web/app/themes/mjh/resources/views/template-press-listing.blade.php
+++ web/app/themes/mjh/resources/views/template-press-listing.blade.php
@@ -4,25 +4,31 @@
 
 @extends('layouts.app')
 @section('content')
-  @include('partials.content-side-press-listing')
-  @while(have_posts()) @php(the_post())
-        @include('partials.content-page')
-        @if($press)
-          @foreach ($press as $press_group)
-            @include('partials.content-press-listing', ['press_group'=>$press_group])
-          @endforeach
-        @else
-          <div style="margin-bottom: 100px;">
-            <div class="alert alert-warning">
-              @php _e("There are no press posts to display","sage"); @endphp
-            </div>
-            {!! get_search_form(false) !!}
+  <div class="row">
+    @if (App::getStickyPosts())
+      @include('partials.content-side-press-listing')
+    @endif
+    @while(have_posts()) @php(the_post())
+          <div class="press-content-main col-md-7"> 
+            @include('partials.content-page')
+            @if($press)
+              @foreach ($press as $press_group)
+                @include('partials.content-press-listing', ['press_group'=>$press_group])
+              @endforeach
+            @else
+              <div style="margin-bottom: 100px;">
+                <div class="alert alert-warning">
+                  @php _e("There are no press posts to display","sage"); @endphp
+                </div>
+                {!! get_search_form(false) !!}
+              </div>
+            @endif
           </div>
+
+        @if ($get_max_num_pages)
+          @include('partials.pagination',['max_num_pages'=>$get_max_num_pages])
         @endif
 
-      @if ($get_max_num_pages)
-        @include('partials.pagination',['max_num_pages'=>$get_max_num_pages])
-      @endif
-
-  @endwhile
+    @endwhile
+  </div>
 @endsection
