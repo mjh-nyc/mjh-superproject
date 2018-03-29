f05a57fa5edfb01a1332136d2966f33366969951
Cyril Tsiboulski <ct@cloudred.com>
2018-03-29 13:35:43 -0400

    Added insight tracking pixel code

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -68,3 +68,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- Yahoo Pixel Tag Code -->
 <script type="application/javascript">(function(w,d,t,r,u){w[u]=w[u]||[];w[u].push({'projectId':'10000','properties':{'pixelId':'10040907'}});var s=d.createElement(t);s.src=r;s.async=true;s.onload=s.onreadystatechange=function(){var y,rs=this.readyState,c=w[u];if(rs&&rs!="complete"&&rs!="loaded"){return}try{y=YAHOO.ywa.I13N.fireBeacon;w[u]=[];w[u].push=function(p){y([p])};y(c)}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr)})(window,document,"script","https://s.yimg.com/wi/ytc.js","dotq");</script>
 <!-- End Yahoo Pixel Tag Code -->
+<!-- Insight Pixel Tag Code -->
+<img height="1" width="1" style="border-style:none;" alt="" src="//insight.adsrvr.org/track/conv/?adv=gloq8w3&ct=0:801ahd6&fmt=3"/>
+<!-- //END Insight Pixel Tag Code -->
