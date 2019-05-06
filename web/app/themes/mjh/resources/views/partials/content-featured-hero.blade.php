403947ccb71c55d8b1b2de20323a720fb7fa2f4a
Cyril Tsiboulski <ct@cloudred.com>
2019-05-06 19:44:31 -0400

    Moved notes and announcements field to below buttons

--- web/app/themes/mjh/resources/views/partials/content-featured-hero.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-featured-hero.blade.php
@@ -10,9 +10,6 @@
 	        @endif
 			</h1>
 
-			<div class="highlighted_exhibition_annonce">
-				<p>{!! App::get_field('exhibition_notes_announcements',App::getCoreExhibitionID()) !!}</p>
-			</div>
 			@if ( App::get_field('exhibition_view_prompt',App::getCoreExhibitionID()) )
 	        	<div class="highlighted_exhibition_button discover">
 	        		<a href="{!! get_the_permalink(App::getCoreExhibitionID()) !!}" class="cta-round cta-outline">{{ App::get_field('exhibition_view_prompt',App::getCoreExhibitionID()) }}</a>
@@ -21,6 +18,10 @@
 
 	        {{-- print buy tickets button (if link added for this exhibition) --}}
 			@include('partials.content-exhibition-ticket-button', ['wrapper_class'=>'highlighted_exhibition_button', 'ticket_url' => App::get_field('exhibition_ticket_button_link', App::getCoreExhibitionID())['url'], 'ticket_url_target'=>App::get_field('exhibition_ticket_button_link', App::getCoreExhibitionID())['target'], 'ticket_url_title'=>App::get_field('exhibition_ticket_button_link', App::getCoreExhibitionID())['title'], 'ticket_url_text'=>App::get_field('exhibition_ticket_button_text',App::getCoreExhibitionID())])
+
+			<div class="highlighted_exhibition_annonce">
+				<p>{!! App::get_field('exhibition_notes_announcements',App::getCoreExhibitionID()) !!}</p>
+			</div>
 
 			{{-- print sponsors --}}
     		@if ( App::get_repeater_field('primary_sponsors_repeater', App::getCoreExhibitionID()) || App::get_repeater_field('secondary_sponsor_header', App::getCoreExhibitionID()) )
