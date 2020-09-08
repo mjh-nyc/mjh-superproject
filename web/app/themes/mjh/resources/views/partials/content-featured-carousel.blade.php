ebcf8c00261634798cb5d1ac1fc8b3c3d75d5e33
Al Sierra <al@cloudred.com>
2020-09-08 01:08:37 -0400

    Refactor homepage exhibition slider to use flexible content items

--- web/app/themes/mjh/resources/views/partials/content-featured-carousel.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-featured-carousel.blade.php
@@ -1,23 +1,24 @@
-@if (App::get_repeater_field('featured_carousel_repeater'))
+@php ($section_title = get_sub_field('exhibitions_section_title'))
+@if (!empty(get_sub_field('featured_carousel_repeater')))
   <div class="wrap carousel container-fluid">
     <div class="hero-area-home">
       <div class="slider-for">
-        @foreach (App::get_repeater_field('featured_carousel_repeater') as $item)
-          @if (get_post_type($item['carousel_item']) == "exhibition")
-            <div class="slide-card lazy" data-src="{{App::get_field('hero_image',$item['carousel_item'])['sizes']['large']}}"><span class="sr-only">{{App::get_field('hero_image',$item['carousel_item'])['alt']}}</span></div>
-          @elseif (get_post_type($item['carousel_item']) == "event")
-            <div class="slide-card lazy" data-src="{{ App::featuredImageSrc('large',$item['carousel_item']) }}"><span class="sr-only">{{ App::featuredImageAlt($item['carousel_item']) }}</span></div>
-          @endif
-
-        @endforeach
+        @while (have_rows('featured_carousel_repeater')) @php(the_row())
+        @php($post_id = get_sub_field('carousel_item'))
+        @if (get_post_type($post_id) == "exhibition")
+          <div class="slide-card lazy" data-src="{{App::get_field('hero_image',$post_id)['sizes']['large']}}"><span class="sr-only">{{App::get_field('hero_image',$post_id)['alt']}}</span></div>
+        @elseif (get_post_type($item['carousel_item']) == "event")
+          <div class="slide-card lazy" data-src="{{ App::featuredImageSrc('large',$post_id) }}"><span class="sr-only">{{ App::featuredImageAlt($post_id) }}</span></div>
+        @endif
+        @endwhile
       </div>
     </div>
     <div class="onview">
-        <div class="header">@php _e("Now on view","sage"); @endphp</div>
+        <div class="header">{!! $section_title !!}</div>
         <div class="featured-carousel  mjh-slider slider-nav container">
-            @foreach (App::get_repeater_field('featured_carousel_repeater') as $item)
-                @include('partials.content-exhibition-card',['item_id' => $item['carousel_item'],'header' => $item['carousel_item_header']])
-            @endforeach
+          @while (have_rows('featured_carousel_repeater')) @php(the_row())
+            @include('partials.content-exhibition-card',['item_id' => get_sub_field('carousel_item'),'header' => get_sub_field('carousel_item_header')])
+          @endwhile
         </div>
     </div>
   </div>
