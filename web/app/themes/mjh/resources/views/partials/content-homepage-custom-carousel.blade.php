18d07243097abad8faccbab1d45a71a603845d09
Cyril Tsiboulski <3483587+mrlosh@users.noreply.github.com>
2020-09-08 19:13:42 -0400

    Adjust padding for carousels with less than 4 slides

--- web/app/themes/mjh/resources/views/partials/content-homepage-custom-carousel.blade.php
+++ web/app/themes/mjh/resources/views/partials/content-homepage-custom-carousel.blade.php
@@ -6,14 +6,17 @@
   </div>
   <div class="custom-carousel row">
     <div class="col-md-12 wrapper">
-      <div class="slider-custom mjh-slider container">
+      @php  $carousel_items = get_sub_field('custom_carousel_section_repeater');
+            $carousel_total = count($carousel_items);
+      @endphp
+      <div class="slider-custom mjh-slider container" @if($carousel_total < 4) style="padding-bottom: 0" @endif>
         @if(!empty(get_sub_field('custom_carousel_section_repeater')))
-            @while (have_rows('custom_carousel_section_repeater')) @php(the_row())
-          @if(empty(get_sub_field('custom_carousel_section_is_custom_content')))
-            @php($item_id = get_sub_field('custom_carousel_section_post')[0])
-          @endif
+          @while (have_rows('custom_carousel_section_repeater')) @php(the_row())
+            @if(empty(get_sub_field('custom_carousel_section_is_custom_content')))
+              @php($item_id = get_sub_field('custom_carousel_section_post')[0])
+            @endif
             <div class="custom-card slide-card">
-              <a href="@if(!empty($item_id)){!! get_the_permalink($item_id); !!}@else {{get_sub_field('custom_carousel_link')}} @endif" class="card-link">
+              <a href="@if(!empty($item_id)){!! get_the_permalink($item_id) !!}@else {{get_sub_field('custom_carousel_link')}} @endif" class="card-link">
                 <div class="card-image">
                   <img src="@asset('images/placeholder.png')" data-lazy="@if(!empty($item_id)){{App::featuredImageSrc('square@2x',$item_id)}}@else {{get_sub_field('custom_carousel_section_content')['callout_image']['sizes']['square@2x'] }} @endif" data-mobilesrc="@if(!empty($item_id)){{App::featuredImageSrc('square@1x',$item_id)}}@else {{get_sub_field('custom_carousel_section_content')['callout_image']['sizes']['square@1x'] }} @endif">
 
@@ -28,7 +31,7 @@
               </a>
             </div>
             @php($item_id=0)
-            @endwhile
+          @endwhile
         @endif
       </div>
     </div>
