0fc00538771f1aba3212fa45edddc4eb8213d7ec
Al Sierra <al@cloudred.com>
2020-09-09 21:38:10 -0400

    Move Google tag manager snippet to header per request by client

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -9,7 +9,7 @@
     </div>
 
     <div class="copyright row">
-      <div class="col-md-12"> 
+      <div class="col-md-12">
         @if (has_nav_menu('footer_navigation'))
           {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'container_class' => 'links']) !!}
         @endif
@@ -28,17 +28,6 @@
 
   </div>
 </footer>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PBW35TT');</script>
-<!-- End Google Tag Manager -->
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBW35TT"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <!--
 Event snippet for MOJH Retargeting Pixel on https://mjhnyc.org/: Please do not remove.
 -->
