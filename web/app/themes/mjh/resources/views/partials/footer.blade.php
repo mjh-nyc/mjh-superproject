848f5ee645396cb8fa61701f0499fa05153a93bb
Al Sierra <al@cloudred.com>
2021-01-26 22:45:06 -0500

    Add facebook pixel code

--- web/app/themes/mjh/resources/views/partials/footer.blade.php
+++ web/app/themes/mjh/resources/views/partials/footer.blade.php
@@ -35,3 +35,22 @@ Event snippet for MOJH Retargeting Pixel on https://mjhnyc.org/: Please do not r
   <img src="https://ad.doubleclick.net/ddm/activity/src=9105301;type=retar0;cat=mojhr0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?" width="1" height="1" alt=""/>
 </noscript>
 <!-- End of event snippet: Please do not remove -->
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '904453223633161');
+  fbq('track', 'PageView');
+</script>
+<noscript>
+  <img height="1" width="1"
+       src="https://www.facebook.com/tr?id=904453223633161&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
