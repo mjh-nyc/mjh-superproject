7d31bc149a4b434aaea5cb826cecbbb5ea3eed08
cr devteam <dev@cloudred.com>
2021-03-31 17:11:26 -0400

    Add in memoriam cateogry rules for blog listing and single post pages

--- web/app/themes/mjh/resources/controllers/views/template-blog-listing.php
+++ web/app/themes/mjh/resources/controllers/views/template-blog-listing.php
@@ -8,7 +8,8 @@ use WP_Query;
 class Blogs extends Controller
 {
     var $paged = 1;
-    var $pressCategory;
+    //var $pressCategory;
+    var $excludeCategories;
     var $blogs;
     /**
      * Constructor
@@ -16,17 +17,33 @@ class Blogs extends Controller
      */
     function __construct()
     {
-        $this->pressCategory= App::getPressCategory('press');
+        //$this->pressCategory= App::getPressCategory('press');
+        //$this->inMemoriamCategory= App::getPressCategory('in-memoriam');
+        //construct an array of items to be excluded, if any
+        //these come from ACF field dropdown attached to the blog listing page template
+        $this->excludeCategories = App::get_field('blog_exclude_items');
+
         $this->paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
     }
 
     /**
-     * Return upcoming events posts
+     * Return latest blog posts (excluding press and in memoriam categories)
      *
      * @return array
      */
     public function blogs()
-    {
+    {     
+        //if in memoriam is not in the exclude array, arrange by title
+        //otherwise arrange by post date
+        if(!in_array(get_category_by_slug('in-memoriam')->term_id, $this->excludeCategories)) {
+            $orderby = 'title';
+            $order = 'ASC';
+        } else {
+            $orderby = 'post_date';
+            $order = 'DESC';
+        }
+        
+
         $currentDate = strtotime('today midnight');
         $pParamHash = array('post_type' => 'post','posts_per_page' => 9,'paged'=>$this->paged);
         $pParamHash['tax_query'] =  array(
@@ -34,12 +51,12 @@ class Blogs extends Controller
             '0'=> array(
                 'taxonomy'	 => 'category',
                 'field'    => 'term_id',
-                'terms'    => array($this->pressCategory->term_id),
+                'terms'    => $this->excludeCategories,
                 'operator' => 'NOT IN',
                 )
         );
-        $pParamHash['orderby']	= 'post_date';
-        $pParamHash['order']	= 'DESC';
+        $pParamHash['orderby']	= $orderby;
+        $pParamHash['order']	= $order;
         $stickyHash = App::getPressStickyPosts();
         if(!empty($stickyHash)){
             $pParamHash['post__not_in'] = $stickyHash;
